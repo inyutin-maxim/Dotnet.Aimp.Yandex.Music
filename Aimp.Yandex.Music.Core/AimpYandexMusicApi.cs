b2cd92060aabd8dc00f2bf428c02ebe9b50cf312
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2020-07-25 01:58:24 +0300

    Добавляет синглтон для Yandex.Music API

---
+++ Aimp.Yandex.Music.Core/AimpYandexMusicApi.cs
@@ -0,0 +1,22 @@
+﻿using Yandex.Music.Api;
+
+namespace Aimp.Yandex.Music.Core
+{
+	public static class AimpYandexMusicApi
+	{
+		private static readonly object padlock = new object();
+
+		private static YandexMusicApi instance;
+
+		public static YandexMusicApi Instance
+		{
+			get
+			{
+				lock (padlock)
+				{
+					return instance ??= new YandexMusicApi();
+				}
+			}
+		}
+	}
+}
