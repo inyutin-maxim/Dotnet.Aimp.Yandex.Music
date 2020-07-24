ab8e7d505e3c52a660d7601761a38830711886c7
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2020-07-25 02:00:04 +0300

    Добавляет форму авторизации (в работе)

---
+++ Aimp.Yandex.Music.UI/SettingsForm.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using AIMP.SDK.Options;
+using AIMP.SDK.Player;
+using Aimp.Yandex.Music.Core;
+
+namespace Aimp.Yandex.Music.UI
+{
+	public partial class SettingsForm : Window
+	{
+		private IAimpOptionsDialogFrame _parentFrame;
+
+		private IAimpPlayer _player;
+
+
+		public SettingsForm(IntPtr parentWindow, IAimpPlayer player, IAimpOptionsDialogFrame parentFrame)
+			: this()
+		{
+			SetParent(new WindowInteropHelper(this).Handle, parentWindow);
+			_parentFrame = parentFrame;
+			_player = player;
+			Show();
+			WindowState = WindowState.Maximized;
+		}
+
+		public SettingsForm()
+		{
+			InitializeComponent();
+		}
+
+		[DllImport("user32.dll", SetLastError = true)]
+		private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+		private void YandexAuthorize(object sender, RoutedEventArgs e)
+		{
+			AimpYandexMusicApi.Instance.Authorize(Login.Text, Password.Password);
+		}
+	}
+}
