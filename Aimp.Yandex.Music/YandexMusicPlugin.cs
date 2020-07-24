4c5326b0f703e1fdc60dcaf715db5cb582985cc5
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2020-07-25 00:29:02 +0300

    Инициализация проекта

---
+++ Aimp.Yandex.Music/YandexMusicPlugin.cs
@@ -0,0 +1,33 @@
+﻿using AIMP.SDK;
+using JetBrains.Annotations;
+
+namespace Aimp.Yandex.Music
+{
+	/// <inheritdoc />
+	[AimpPlugin(PluginName,
+		PluginAuthor,
+		PluginVersion,
+		AimpPluginType = AimpPluginType.Addons,
+		Description = PluginDescription)]
+	[UsedImplicitly]
+	public class YandexMusicPlugin : AimpPlugin
+	{
+		private const string PluginName = "Aimp.Yandex.Music";
+
+		private const string PluginVersion = "1.0.0";
+
+		private const string PluginDescription = "Поддержка сервиса Yandex.Music";
+
+		private const string PluginAuthor = "inyutin-maxim";
+
+		/// <inheritdoc />
+		public override void Initialize()
+		{
+		}
+
+		/// <inheritdoc />
+		public override void Dispose()
+		{
+		}
+	}
+}
